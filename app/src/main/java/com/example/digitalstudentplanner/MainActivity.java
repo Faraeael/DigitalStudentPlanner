89453c0b691da38bfe0ade9425f6bc545bd3b4d2
Faraeal <talatalamichaeljr@gmail.com>
2022-12-14 18:53:17 +0800

    Ver 1.0.2
    Added Month to Planner

--- app/src/main/java/com/example/digitalstudentplanner/MainActivity.java
+++ app/src/main/java/com/example/digitalstudentplanner/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.digitalstudentplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button planner;
+    Button createSched;
+    Button Sched;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        planner =findViewById(R.id.PlannerBtn);
+        createSched = findViewById(R.id.createSchedBtn);
+        Sched = findViewById(R.id.scheduleBtn);
+
+        planner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CalendarPlanner.class));
+            }
+        });
     }
 }
