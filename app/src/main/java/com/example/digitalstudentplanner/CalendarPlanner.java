a8591efec4d9d3cd58f9fda242caa934e81489dc
Faraeal <talatalamichaeljr@gmail.com>
2022-12-16 18:26:59 +0800

    Ver 1.1.0
    Planner 90% done

--- app/src/main/java/com/example/digitalstudentplanner/CalendarPlanner.java
+++ app/src/main/java/com/example/digitalstudentplanner/CalendarPlanner.java
@@ -1,9 +1,13 @@
 package com.example.digitalstudentplanner;
 
+import static com.example.digitalstudentplanner.CalendarUtils.daysInMonthArray;
+import static com.example.digitalstudentplanner.CalendarUtils.monthYearFromDate;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -18,17 +22,17 @@ public class CalendarPlanner extends AppCompatActivity implements CalendarAdapte
 {
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
-    private LocalDate selectedDate;
+
 
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) 
+    protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_planner);
         initWidgets();
-        selectedDate = LocalDate.now();
+        CalendarUtils.selectedDate = LocalDate.now();
         setMonthView();
     }
 
@@ -40,8 +44,8 @@ public class CalendarPlanner extends AppCompatActivity implements CalendarAdapte
 
     private void setMonthView()
     {
-        monthYearText.setText(monthYearFromDate(selectedDate));
-        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> daysInMonth = daysInMonthArray();
 
         CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
@@ -49,54 +53,28 @@ public class CalendarPlanner extends AppCompatActivity implements CalendarAdapte
         calendarRecyclerView.setAdapter(calendarAdapter);
     }
 
-    private ArrayList<String> daysInMonthArray(LocalDate date)
-    {
-        ArrayList<String> daysInMonthArray = new ArrayList<>();
-        YearMonth yearMonth = YearMonth.from(date);
-        int daysInMonth = yearMonth.lengthOfMonth();
-
-        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
-        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
-
-        for(int i = 1; i <= 42; i++)
-        {
-            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek)
-            {
-                daysInMonthArray.add("");
-            }
-            else
-            {
-                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
-            }
-        }
-        return daysInMonthArray;
-    }
-
-    private String monthYearFromDate(LocalDate date)
-    {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
-        return date.format(formatter);
-    }
-
     public void previousMonthAction(View view)
     {
-        selectedDate = selectedDate.minusMonths(1);
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusMonths(1);
         setMonthView();
     }
 
     public void nextMonthAction(View view)
     {
-        selectedDate = selectedDate.plusMonths(1);
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusMonths(1);
         setMonthView();
     }
 
-    @Override
-    public void onItemClick(int position, String dayText)
-    {
-        if(dayText.equals(""))
+    public void onItemClick(int position, LocalDate date) {
+        if (date != null)
         {
-            String message = "Selected Date" + dayText + " " + monthYearFromDate(selectedDate);
-            Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+        CalendarUtils.selectedDate = date;
+        setMonthView();
         }
+    }
+
+    public void weeklyAction(View view)
+    {
+        startActivity(new Intent(this,WeekViewActivity.class));
     }
 }
